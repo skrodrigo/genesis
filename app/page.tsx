893e3c41752bd66fbba3f8ea6e422376af4a7303
skrodrigo <rodrigoa0987@gmail.com>
2025-06-30 22:29:32 -0300

    feat: add auth in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-    </div>
-  )
+import { headers } from "next/headers"
+import { auth } from "./utils/auth"
+import { redirect } from "next/navigation"
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session) {
+    redirect("/landing")
+  }
+
+  return
 }
