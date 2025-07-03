463f0a2ed1e9554baf08b3db66e16b343438fe1d
skrodrigo <rodrigoa0987@gmail.com>
2025-07-03 09:01:50 -0300

    fix: middleware all routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { auth } from "./app/utils/auth";
+import { auth } from "./lib/auth";
  
 export async function middleware(request: NextRequest) {
     const session = await auth.api.getSession({
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
  
 export const config = {
   runtime: "nodejs",
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard(.*)"],
 };
