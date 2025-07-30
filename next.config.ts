4a4ae280c2644f8eaf7c857a88aa4bd410af514b
skrodrigo <rodrigoa0987@gmail.com>
2025-07-30 10:53:06 -0300

    fix: remove not used functions in next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-    async rewrites() {
-    		return [
-    			{
-    				source: '/api/c15t/:path*',
-    				destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
-    			},
-    		];
-    	}
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
